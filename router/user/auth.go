70bf3f487adb893e814a7ef760946c37b10d9c8c
Rustami Barzu <programmer14@HUMO.local>
2020-02-24 13:54:17 +0500

    add small changes

--- router/user/auth.go
+++ router/user/auth.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"fmt"
+	"foodShop/dbr"
 	"foodShop/utils"
 	"log"
 
@@ -8,6 +10,7 @@ import (
 )
 
 func Authenticate(userName string, password string, c *gin.Context) (string, bool) {
+	Db = dbr.GetDb()
 	var user User
 	log.Println("--------------------->", password)
 	if result := Db.Where("userName = ? and disabled = false", userName).
@@ -22,4 +25,29 @@ func Authenticate(userName string, password string, c *gin.Context) (string, boo
 	}
 
 	return "", false
+}
+
+func Payload(userName string) map[string]interface{} {
+	// var userR models.Usersinroles
+	var user User
+
+	if result := Db.Table("tusers u,troles r").
+		Where("u.role = r.role_id and u.userName = ?", userName).
+		Select("u.*, r.role_name as role").
+		Scan(&user).Limit(1); result.Error != nil {
+		fmt.Println(result.Error.Error())
+		return map[string]interface{}{
+			"userID":   0,
+			"role":     "undefined",
+			"userName": "0",
+		}
+	}
+
+	log.Println("userId= ", user.ID, "\nuserName = ", user.Username, "\nrole = ", user.Role)
+
+	return map[string]interface{}{
+		"userID":   user.ID,
+		"role":     user.Role,
+		"userName": user.Username,
+	}
 }
